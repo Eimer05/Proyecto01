ec12cbdfb0d8540bd8efa828825f2d8552100293
Eimer Armijos <eaarmijos8@utpl.edu.ec>
2023-10-24 22:20:28 -0500

    Segundo commit

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -14,6 +14,8 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Hola Mundo");
+        
         // TODO code application logic here
     }
     
